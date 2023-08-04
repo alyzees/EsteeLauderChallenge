7e1dd881adf29c6caa4590d56c6287ebb0b725f1
Alyzee Sosa <69488327+alyzees@users.noreply.github.com>
2023-08-04 00:12:27 -0700

    Initial Commit

---
+++ EsteeLauderChallenge/EsteeLauderChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  EsteeLauderChallengeApp.swift
+//  EsteeLauderChallenge
+//
+//  Created by Alyzee Sosa on 8/4/23.
+//
+
+import SwiftUI
+
+@main
+struct EsteeLauderChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
